1b4a8766fe5c2fb2946c1e05157a7689ce309088
RShandal <141106095+RShandal@users.noreply.github.com>
2023-08-16 19:42:43 -0400

    Add files via upload
    Added simple temperature conversion

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 // window.alert("Note: This site is currently under construction!");
 
-let zipcode;
+/*let zipcode;
 
-document.getElementById("cityZipSearch").onclick = function() {
+document.getElementById("cityZipSearchBtn").onclick = function() {
     zipcode = document.getElementById("zipcode").value;
     console.log(zipcode);
+}*/
+
+document.getElementById("submitButton").onclick = function() {
+    
+    let temp;
+
+    if(document.getElementById("cButton").checked) {
+        temp = document.getElementById("tempBox").value;
+        temp = Number(temp);
+        temp = toCelsius(temp);
+        document.getElementById("tempLabel").innerHTML = temp + "°C";
+    }
+    else if(document.getElementById("fButton").checked){
+        temp = document.getElementById("tempBox").value;
+        temp = Number(temp);
+        temp = toFahrenheit(temp);
+        document.getElementById("tempLabel").innerHTML = temp + "°F";
+    }
+    else{
+        document.getElementById("tempLabel").innerHTML = "Please select a unit of conversion!";
+    }
+
+
+}
+
+function toCelsius(temp){
+    return (temp - 32) * (5/9);
+}
+
+function toFahrenheit(temp){
+    return temp * 9 / 5 + 32;
 }
