69a999d5bb4e43f17365ba3c98f4db1ecd142f44
RShandal <141106095+RShandal@users.noreply.github.com>
2023-08-16 01:24:00 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// window.alert("Note: This site is currently under construction!");
+
+let zipcode;
+
+document.getElementById("cityZipSearch").onclick = function() {
+    zipcode = document.getElementById("zipcode").value;
+    console.log(zipcode);
+}
